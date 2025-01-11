bb86575b811b00234504e0ef55e16f5aef6d711f
Lolo Aboufoul <laboufou@uncc.edu>
2025-01-11 06:56:53 -0500

    Add files via upload

---
+++ replacement.js
@@ -0,0 +1,13 @@
+// Get all the elements that you care about into an array
+//let elements = Array.prototype.slice.call(document.querySelectorAll("p, h1, header"));
+const elements = document.querySelectorAll('*');
+let arr = Array.from(elements);
+
+function swap(){
+  arr.forEach(function(el){
+    // Do the replace on the element
+    el.textContent = el.textContent.replace('I', 'We');
+  });
+};
+
+
