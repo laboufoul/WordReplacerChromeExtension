a2777d2b984d91ada638f5111302a49b0ad0aec1
Lolo Aboufoul <laboufou@uncc.edu>
2025-02-10 11:52:34 -0500

    Update content.js

--- content.js
+++ content.js
@@ -1,14 +1,11 @@
-// Get all the elements that you care about into an array
-//let elements = Array.prototype.slice.call(document.querySelectorAll("p, h1, header"));
-const elements = document.querySelectorAll('*');
-let arr = Array.from(elements);
+const text = document.getElementsByTagName("a");
 
-function swap(){
-  arr.forEach(function(el){
-    // Do the replace on the element
-    el.textContent = el.textContent.replace('This', 'coding');
-  });
-};
+for (let i=0; i < text.length; i++){
+  if (text[i].innerHTML.includes('amazon')){
+    text[i].innerHTML = text[i].innerHTML.replace('amazon', 'https://www.etsy.com/' )
+  }
+}
+
 
 
 
